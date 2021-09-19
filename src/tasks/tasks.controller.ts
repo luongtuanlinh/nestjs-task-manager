7c937684d31749a7c14f2a9edb8f8d722c69b5a3
linhlt311 <ltlinh311@gmail.com>
2021-09-19 20:24:02 +0700

    Add task model

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+}
