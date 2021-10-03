e741f0ce2c384899f4da9f99f933852ba1cef76e
linhlt311 <ltlinh311@gmail.com>
2021-10-03 16:57:11 +0700

    Connect db

--- src/tasks/dto/filter-tasks.dto.ts
+++ src/tasks/dto/filter-tasks.dto.ts
@@ -1,6 +1,7 @@
-import { IsOptional, NotEquals } from "class-validator";
+import { IsOptional, IsString } from 'class-validator';
 
 export class FilterTasksDto {
   @IsOptional()
+  @IsString()
   search: string;
 }
